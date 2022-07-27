1b70150622f4053000ba13a7ac387be46e4b61c3
김보경 <rawoon@kakao.com>
2022-07-27 12:03:12 +0900

    dev 브랜치 생성

--- src/components/ActivityTabs.js
+++ src/components/ActivityTabs.js
@@ -5,14 +5,15 @@ import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import MyPhotos from './MyPhotos';
-import MyCalendar from './MyCalendar';
+// import MyCalendar from './MyCalendar';
+import { Calender } from './Calender';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
   return (
     <div
-      role="tabpanel"
+      role="tabpanel" 
       hidden={value !== index}
       id={`simple-tabpanel-${index}`}
       aria-labelledby={`simple-tab-${index}`}
@@ -63,7 +64,7 @@ export default function ActivityTabs() {
       </TabPanel>
       <TabPanel value={value} index={1}>
         내 활동 내역 달력으로 보기
-        <MyCalendar/>
+        <Calender/>
       </TabPanel>
       <TabPanel value={value} index={2}>
         즐겨찾기 목록
